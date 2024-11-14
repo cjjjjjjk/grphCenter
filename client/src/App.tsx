23687eb8049449bc483c403e75e396a601227ed3
cjjjjjjk <ho.vanhai03062004@gmail.com>
2024-11-15 00:41:58 +0700

    add tailwindcss, visx network graph

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
+import Home from './page/Home';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        graphCenter
+    <div>
+      <header className="h-[72px] border-black border-b-2 flex items-center justify-center  " >
+        <h1 className="text-center text-4xl justify-center">Welcome to grphCenter</h1>
       </header>
+      <main>
+        <h2>--- Welcome page !</h2>
+        <Home />
+      </main>
     </div>
   );
 }
