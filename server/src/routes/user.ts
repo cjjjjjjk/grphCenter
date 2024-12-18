a7b1a3d216fd7d6600647899813d4700c9b59af0
cjjjjjjk <ho.vanhai03062004@gmail.com>
2024-12-18 15:24:23 +0700

    update user api: register / login

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,13 +1,27 @@
 import { Request, Response, Router } from "express";
 import Users from "../model/Users";
 
-const userRoutes = Router()
+import bcrypt from 'bcrypt'
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken'
+dotenv.config();
 
-// Register new user
+const userRoutes = Router()
+// Hash password ============================================
+const HashPassword = async function (textPassword: string) {
+    const salt = bcrypt.genSaltSync(Number(process.env.SALTROUNDS))
+    const hash: string = bcrypt.hashSync(textPassword, salt)
+    return hash
+}
+
+
+// Register new user ========================================
 userRoutes.post("/create", async (req: Request, res: Response) => {
     const { username, password, email } = req.body;
     try {
-        const newUser = new Users({ username, password, email })
+        const passWord_hasshed = await HashPassword(password)
+        const newUser = new Users({ username, password: passWord_hasshed, email })
+
         await newUser.save()
         res.status(201).json({
             success: true,
@@ -19,6 +33,38 @@ userRoutes.post("/create", async (req: Request, res: Response) => {
             success: false,
             message: err instanceof Error ? err.message : "Error creating user",
         });
+    }
+})
+
+// Login ====================================================
+userRoutes.post("/login", async (req: Request, res: Response) => {
+    const { username, password } = req.body;
+    try {
+        if (!password) throw new Error('Password can not be empty !')
+
+        const user = await Users.findOne({ username })
+        if (!user) throw new Error(`No user match with username: ${username}`)
+        const checkPassword = bcrypt.compareSync(password, user?.password)
+
+
+        if (!checkPassword) throw new Error('Compare password false !')
+
+        const user_token = jwt.sign({
+            _id: user._id,
+            username: user.username,
+            useremail: user.email,
+        }, `${process.env.SCRETKEY || "abc-123"}`, { expiresIn: `${process.env.TOKENEXPIRED || "1h"}` })
+
+        res.json({
+            success: true,
+            message: "Login sucess !",
+            token: user_token
+        })
+    } catch (err) {
+        res.json({
+            success: false,
+            message: err instanceof Error ? err.message : 'Server: Login false',
+        })
     }
 })
 
